70adb534de430ec93bee3f92e630cba1c71fdabf
Caio Bruno <caiobruno90@yahoo.com>
2023-10-19 12:11:47 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/caiobruno/whorkshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caiobruno.whorkshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

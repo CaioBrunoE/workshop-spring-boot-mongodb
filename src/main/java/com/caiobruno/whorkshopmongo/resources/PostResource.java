716c88faac956a6b3ae6073290e5983df7fd6cb7
Caio Bruno <caiobruno90@yahoo.com>
2023-10-20 14:23:27 -0300

    Obtendo um post por id

---
+++ src/main/java/com/caiobruno/whorkshopmongo/resources/PostResource.java
@@ -0,0 +1,28 @@
+package com.caiobruno.whorkshopmongo.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.caiobruno.whorkshopmongo.domain.Post;
+
+import com.caiobruno.whorkshopmongo.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+	@Autowired
+	private PostService service;
+    
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id){
+		Post obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
+
+}
---
+++ src/main/java/com/caiobruno/whorkshopmongo/services/PostService.java
@@ -0,0 +1,23 @@
+package com.caiobruno.whorkshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.caiobruno.whorkshopmongo.domain.Post;
+import com.caiobruno.whorkshopmongo.repository.PostRepository2;
+import com.caiobruno.whorkshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository2 repository;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+}

a47d6243380c3f7be2fc967fc89902695c9cacf8
Caio Bruno <caiobruno90@yahoo.com>
2023-10-20 10:10:54 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/com/caiobruno/whorkshopmongo/config/Instantiation.java
+++ src/main/java/com/caiobruno/whorkshopmongo/config/Instantiation.java
@@ -15,19 +15,13 @@ public class Instantiation implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 	
-	
 	@Override
 	public void run(String... args) throws Exception {
-		
-		userRepository.deleteAll();
-		
+		userRepository.deleteAll();	
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
 		userRepository.saveAll(Arrays.asList(maria,alex,bob));
-		
-		
 	}
 
 }

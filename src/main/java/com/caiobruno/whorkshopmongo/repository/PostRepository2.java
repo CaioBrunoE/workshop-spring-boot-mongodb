088dd6f4bb729f1b917f07b27610b00d174deac2
Caio Bruno <caiobruno90@yahoo.com>
2023-10-20 15:27:37 -0300

    Consulta simples com @Query

--- src/main/java/com/caiobruno/whorkshopmongo/repository/PostRepository2.java
+++ src/main/java/com/caiobruno/whorkshopmongo/repository/PostRepository2.java
@@ -3,6 +3,7 @@ package com.caiobruno.whorkshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.caiobruno.whorkshopmongo.domain.Post;
@@ -10,6 +11,8 @@ import com.caiobruno.whorkshopmongo.domain.Post;
 @Repository
 public interface PostRepository2 extends MongoRepository<Post, String> {
 
+	@Query("{ 'title': { $regex: ?0 , $options: 'i' } }")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase (String text);
 	
--- src/main/java/com/caiobruno/whorkshopmongo/services/PostService.java
+++ src/main/java/com/caiobruno/whorkshopmongo/services/PostService.java
@@ -23,7 +23,7 @@ public class PostService {
 	
 	public  List<Post> findByTitle(String text){
 	  
-	     return repository.findByTitleContainingIgnoreCase(text);
+	     return repository.searchTitle(text);
 	}
 	
 }
